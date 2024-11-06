ee0c88fdcb8041c80361367583c010893f2b3f9a
younes makhchan <makhchaney@cyberdefenders.org>
2024-11-06 18:39:19 +0100

    remove the check

--- tools.py
+++ tools.py
@@ -16,8 +16,6 @@ def update_company_value( company_name, key, new_value):
 
 
 def validate_companies_file(file_path="./companies.json"):
-    if not os.path.exists(file_path):
-        raise FileNotFoundError(f"The file '{file_path}' does not exist.")
     with open(file_path, 'r') as file:
         data = json.load(file)
     
